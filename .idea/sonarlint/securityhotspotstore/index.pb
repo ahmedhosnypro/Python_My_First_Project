b9fabd914eec701af103f0c3cd217e8bffd32f5e
Ahmed Hosny <ahhosnyas@gmail.com>
2023-08-02 19:49:38 +0300

    Stage 2/3: Measuring total income

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+V
+&My First Project/task/test/__init__.py,6\4\647f38995ce9c84a2632f4ac58c7573d313fe3e6
--- My First Project/task/calculator.py
+++ My First Project/task/calculator.py
@@ -1,7 +1,16 @@
-print("Prices:")
-print("Bubblegum: $2")
-print("Toffee: $0.2")
-print("Ice cream: $5")
-print("Milk chocolate: $4")
-print("Doughnut: $2.5")
-print("Pancake: $3.2")
+bubblegum = 202
+toffee = 118
+ice_cream = 2250
+milk_chocolate = 1680
+doughnut = 1075
+pancake = 80
+
+print("Earned amount:")
+print("Bubblegum: $", bubblegum)
+print("Toffee: $", toffee)
+print("Ice cream: $", ice_cream)
+print("Milk chocolate: $", milk_chocolate)
+print("Doughnut: $", doughnut)
+print("Pancake: $", pancake)
+print()
+print("Income: $", bubblegum + toffee + ice_cream + milk_chocolate + doughnut + pancake)
